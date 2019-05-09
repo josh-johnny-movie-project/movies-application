f8793be11f83367cd41847672e72b496278b0239
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-05-09 08:43:34 -0500

    added some styling and update function

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ sayHello('World');
 const appendMovieData = (title, rating) => {
   var html = "";
   html += `<div class='movieCard'>`;
-  html += `<h2>${title}</h2>`;
-  html += `<p>${rating}</p>`;
+  html += `<h4>${title}</h4>`;
+  html += `<p>Rating: `  + `${rating}</p>`;
   html += `</div>`;
   return html
 };
@@ -21,17 +21,22 @@ const $ = require("jquery");
 
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-    $('#add-movies').append(appendMovieData(title, rating));
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+$(function() {
+  upDateMovies();
 });
 
+const upDateMovies = () => {
+  getMovies().then((movies) => {
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+      console.log(`id#${id} - ${title} - rating: ${rating}`);
+      $('#add-movies').append(appendMovieData(title, rating));
+    });
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
+};
 
 
 $("#submit").click(function () {
@@ -49,7 +54,7 @@ const options = {
   body: JSON.stringify(newMovie),
 };
   fetch(url, options)
-      .then()
+      .then(upDateMovies)
       .catch();
 });
 
